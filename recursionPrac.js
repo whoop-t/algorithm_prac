b43a903256aac32036550c5429063eb227914aac
Tommy Mallis <tommymallis@gmail.com>
2020-02-05 07:30:54 -0800

    Added Fibonacci

--- recursionPrac.js
+++ recursionPrac.js
@@ -2,32 +2,32 @@
 
 //Mimic Math.pow() with recrusion
 
-// const power = (base, expo) => {
-//   //Set base case to exit
-//   if (expo === 1) {
-//     return base;
-//   }
-//   if (expo === 0) {
-//     return 1;
-//   }
-//   //Recursively multiply by base by itself till reaching base case
-//   return base * power(base, expo - 1);
-// };
+const power = (base, expo) => {
+  //Set base case to exit
+  if (expo === 1) {
+    return base;
+  }
+  if (expo === 0) {
+    return 1;
+  }
+  //Recursively multiply by base by itself till reaching base case
+  return base * power(base, expo - 1);
+};
 
-// console.log(power(2, 0));
+console.log(power(2, 0));
 
 //Recursively get factorial
 
-// const factorial = num => {
-//   //Set base case
-//   if (num === 1 || num === 0) {
-//     return 1;
-//   }
-//   //Multiply num by itself minus 1
-//   return num * factorial(num - 1);
-// };
+const factorial = num => {
+  //Set base case
+  if (num === 1 || num === 0) {
+    return 1;
+  }
+  //Multiply num by itself minus 1
+  return num * factorial(num - 1);
+};
 
-// console.log(factorial(0));
+console.log(factorial(0));
 
 //Multiply up all numbers in array
 
@@ -39,3 +39,21 @@ const productOfArray = arr => {
 };
 
 console.log(productOfArray([1, 2, 3]));
+
+//Recieve number and add up all numbers before and including that number
+
+const recursiveRange = num => {
+  if (num === 1) return 1;
+  return num + recursiveRange(num - 1);
+};
+
+console.log(recursiveRange(10));
+
+//Fibonacci recrusively, num is the number in the Fibonacci sqeuence you want returned
+
+const fib = n => {
+  if (n <= 2) return 1;
+  return fib(n - 1) + fib(n - 2);
+};
+
+//* (https://stackoverflow.com/questions/8845154/how-does-the-fibonacci-recursive-function-work)
