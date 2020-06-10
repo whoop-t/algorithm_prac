c25f85a89df57892b5556d7e68dbfc134762ae60
whoop-t <tommymallis@gmail.com>
2020-06-09 20:25:55 -0700

    Updated notes

---
+++ bigO.js
@@ -0,0 +1,18 @@
+/**
+ * * BigO
+ * How we measure speed/memory used with our code
+ * Time Complexity - Time it takes
+ * Space Complexity - Memory it uses
+ *
+ * * Time/Space Complexity
+ * O(1) - as n grow, no change, constant
+ * O(log n) - next best
+ * O(n) - linear growth, grows as n grows
+ * O(nlog n) - not that great
+ * O(n^2) -  quadratic/exponential growth(nested loop)
+ *
+ *
+ * * Space Complexity
+ * Most primitives are constant space
+ * String, reference types(arrays, objects) require O(n) - n is length
+ */
---
+++ problemSolving.js
@@ -0,0 +1,34 @@
+/**
+ * * Problem Solving
+ *
+ * ========================
+ * * Understand the Problem
+ * ========================
+ * Can you restate the problem in your own words?
+ * What are the inputs that go into the problem? What are the outputs?
+ * Can outputs be determined from the inputs?
+ * How should I label the importnat pieces of data?
+ *
+ * ===========================
+ * * Explore Concrete Examples
+ * ===========================
+ * Start with a simple examples, input and output
+ * Progress to more complex examples
+ * Explore Edge Cases
+ * Explore Invalid Inputs
+ *
+ * ===============
+ * * Break It Down
+ * ===============
+ * Write out steps you need to take, forces you to think
+ *
+ * ================
+ * * Solve/Simplify
+ * ================
+ * Try and solve, or simplify solution if not 100%
+ *
+ * ========================
+ * * Look Back and Refactor
+ * ========================
+ * Look back at solution and see what could be done better, what can be refactored
+ */
