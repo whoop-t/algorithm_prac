51e48a354cf9f8850ce10cbd62b9b8edfd228726
whoop-t <tommymallis@gmail.com>
2020-06-10 21:08:52 -0700

    Added useFul pattern notes

---
+++ usefulPatterns.js
@@ -0,0 +1,28 @@
+/**
+ * ==================
+ * Frequency Counter
+ * ==================
+ * Use an Object or set to collect values/frequencies of values
+ * Useful for avoiding nested loops, use multiple single loops
+ *
+ * Example: To compare 2 arrays without nested loops, create 2 objects from those arrays with the frequency of items and comepare in induvidual loops
+ *
+ * ==================
+ * Multiple Pointers
+ * ==================
+ * Create 2 or more pointers or values that corresponds to and index or position, move in direction based on condition
+ *
+ * Example: Get 2 nums in sorted array that add to equal zero, create 2 pointers at beginning and end that converge to find solution
+ *
+ * ==================
+ * Sliding Window
+ * ==================
+ * Use "Window" to slide through collection to get solution
+ *
+ * Example: Find 2 nums in array that when added produce the largest value of the rest
+ *
+ * ==================
+ * Divide and Conquer
+ * ==================
+ * Divide data set into smaller chunks and then repeat process to find solution
+ */
